71df5fc46d2fbc326586dfc5c798b4ab984f9c03
Cameron Alizadeh <cdalizadeh@gmail.com>
2018-09-17 02:43:38 -0400

    fixing css

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import Modal from "./components/Modal.jsx";
 import CanvasContainer from "./components/CanvasContainer.jsx";
-import css from './reset.css';
 import {glue} from './glue.js';
 
 var handlers = {};
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 var config = {
-	mode: "none",
+	mode: "development",
 	entry: path.join(__dirname, "src", "App.jsx"),
 	output: {
 		path: path.join(__dirname, "dist"),
